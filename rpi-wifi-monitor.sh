9e1ffb61f987c2aad393bed7c058cfe5a0445ddc
yamato225 <yamato0024@hotmail.com>
2019-09-07 18:37:43 +0900

    fix for dhcpcd

--- rpi-wifi-monitor.sh
+++ rpi-wifi-monitor.sh
@@ -3,7 +3,9 @@
 cd /opt/rpi-wifi-monitor
 source rpi-wifi-monitor.conf
 
-# check wpa_supplicant.conf 
+WIFI_IP=""
+
+# check wpa_supplicant.conf
 # return 0 if wpa_supplicant.conf is not set.
 check_config_file() {
     echo $(grep "ssid" /etc/wpa_supplicant/wpa_supplicant.conf | wc -l)
@@ -12,6 +14,7 @@ check_config_file() {
 # check connection.
 # return 0 if ping command failed repeatedly.
 check_connection() {
+    WIFI_IP=`hostname -I | awk '{print $1}'`
     for (( i=0; i < $CHECK_CONNECTION_TRY_NUM ; i++ ))
     do
         ping $CHECK_CONNECTION_TARGET_IP -c 1 > /dev/null
@@ -23,45 +26,54 @@ check_connection() {
 
 off_ap() {
     echo "off_ap"
+    #ip addr del 172.24.1.1 dev wlan0
     [ $(pgrep hostapd | wc -l) -gt 0 ] && killall hostapd
     systemctl stop dnsmasq
     systemctl stop dhcpcd
-    ifconfig wlan0 down
-    grep -v "AP CONFIG" /etc/dhcpcd.conf > /tmp/dhcpcd.conf.tmp
-    cp /tmp/dhcpcd.conf.tmp /etc/dhcpcd.conf
 }
 
 on_ap() {
     echo "on_ap"
     # set static address to wlan0
-    cat << EOL | tee -a /etc/dhcpcd.conf
-interface wlan0                        #AP CONFIG
-static ip_address=172.24.1.1/24        #AP CONFIG
-static routers=172.24.1.1              #AP CONFIG
-static domain_name_servers=172.24.1.1  #AP CONFIG
-static broadcast 172.24.1.255          #AP CONFIG
+    if [ $(grep AP_CONFIG /etc/dhcpcd.conf | wc -l) -lt 1 ];then
+	cat << EOL | tee -a /etc/dhcpcd.conf
+interface wlan0                        #AP_CONFIG
+static ip_address=172.24.1.1/24        #AP_CONFIG
+static routers=172.24.1.1              #AP_CONFIG
+static domain_name_servers=172.24.1.1  #AP_CONFIG
+static broadcast 172.24.1.255          #AP_CONFIG
 EOL
-    ifconfig wlan0 up
+    fi
     systemctl daemon-reload
     systemctl restart dhcpcd
     # remove static address config for wifi
+    while true
+    do
+        [ $(hostname -I | grep 172.24.1.1 | wc -l) -gt 0 ] && break
+        echo "preparing to restart adaptor"
+        sleep 1
+    done
     systemctl restart dnsmasq
-    # set ssid name = hostname
+    bash -c "sleep 3 && systemctl restart avahi-daemon" &
     sed -i -e "s/ssid=\(.*\)/ssid=$(hostname)ap/g" /etc/hostapd/hostapd.conf
     hostapd /etc/hostapd/hostapd.conf
 }
 
 off_wifi() {
     echo "off_wifi"
+    ip addr del $WIFI_IP dev wlan0
+    [ -e /opt/rpi-wifi-monitor/10-wpa_supplicant ] || cp -f /lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant /opt/rpi-wifi-monitor/
+    rm -f /lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant
     systemctl daemon-reload
     systemctl stop wpa_supplicant
     systemctl stop dhcpcd
-    ifconfig wlan0 down
 }
 
 on_wifi() {
     echo "on_wifi"
-    ifconfig wlan0 up
+    grep -v "AP_CONFIG" /etc/dhcpcd.conf > /tmp/dhcpcd.conf.tmp
+    cp /tmp/dhcpcd.conf.tmp /etc/dhcpcd.conf
+    cp /opt/rpi-wifi-monitor/10-wpa_supplicant /lib/dhcpcd/dhcpcd-hooks/
     systemctl daemon-reload
     systemctl restart dhcpcd
     systemctl restart wpa_supplicant
